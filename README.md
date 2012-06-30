6de34d4b40ad72f2e64d48672b31a312d883bf32
Timothy Armstrong <armstrong.timothy@gmail.com>
2012-06-30 13:17:56 -0700

    Adding old files

---
+++ README.md
@@ -0,0 +1,4 @@
+locator
+=======
+
+Experimental mobile webapp (and perhaps native app at some point) to ease the process of two friends finding each other.
---
+++ client/js/js.js
@@ -0,0 +1,40 @@
+var handleNewPosition = function(e) {
+  var longitudeElement = document.querySelector('#debug .debug-longitude');
+  var latitudeElement = document.querySelector('#debug .debug-latitude');
+  var accuracyElement = document.querySelector('#debug .debug-locationaccuracy');
+  
+  longitudeElement.innerText = e.coords.longitude;
+  latitudeElement.innerText = e.coords.latitude;
+  accuracyElement.innerText = e.coords.accuracy;
+};
+
+var handleNewOrientation = function(e) {
+  var alphaElement = document.querySelector('#debug .debug-alpha');
+  var betaElement = document.querySelector('#debug .debug-beta');
+  var gammaElement = document.querySelector('#debug .debug-gamma');
+  
+  var compassHeadingElement =
+      document.querySelector('#debug .debug-compassheading');
+  var compassAccuracyElement =
+      document.querySelector('#debug .debug-compassaccuracy');
+  
+  alphaElement.innerText = e.alpha;
+  betaElement.innerText = e.beta;
+  gammaElement.innerText = e.gamma;
+  
+  compassHeadingElement.innerText = e.webkitCompassHeading;
+  compassAccuracyElement.innerText = e.webkitCompassAccuracy;
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  
+  // Feature detection.
+  if (!navigator.geolocation) {
+    alert('No geolocation available');
+    return;
+  }
+  
+  navigator.geolocation.watchPosition(handleNewPosition, null,
+      { enableHighAccuracy: true, maximumAge: 5000, timeout: 27000});
+  
+}, false);
