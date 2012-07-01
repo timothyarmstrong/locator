19cafc441b499485919299a3bfd6c44653788b95
Timothy Armstrong <armstrong.timothy@gmail.com>
2012-06-30 20:11:39 -0700

    Adding some body html

--- client/js/js.js
+++ client/js/js.js
@@ -1,7 +1,8 @@
 var handleNewPosition = function(e) {
   var longitudeElement = document.querySelector('#debug .debug-longitude');
   var latitudeElement = document.querySelector('#debug .debug-latitude');
-  var accuracyElement = document.querySelector('#debug .debug-locationaccuracy');
+  var accuracyElement =
+      document.querySelector('#debug .debug-locationaccuracy');
   
   longitudeElement.innerText = e.coords.longitude;
   latitudeElement.innerText = e.coords.latitude;
@@ -34,9 +35,11 @@ document.addEventListener('DOMContentLoaded', function() {
     return;
   }
   
+  /*
   navigator.geolocation.watchPosition(handleNewPosition, null,
       { enableHighAccuracy: true, maximumAge: 5000, timeout: 27000});
   
   window.addEventListener('deviceorientation', handleNewOrientation, false);
+  */
   
 }, false);
