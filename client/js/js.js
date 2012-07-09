7db01c3717d79e3974f975ea6f28ca58685e4dcf
Timothy Armstrong <armstrong.timothy@gmail.com>
2012-07-08 23:20:13 -0700

    Adding a dummy router, and some empty views and models

--- client/js/js.js
+++ client/js/js.js
@@ -54,3 +54,122 @@ document.addEventListener('DOMContentLoaded', function() {
   });
   
 }, false);
+
+
+// MODELS
+
+var Connection = Backbone.Model.extend({
+  initialize: function() {
+    // establish websocket connection, fire connected event
+  }
+});
+
+var LocationData = Backbone.Model.extend({
+  initialize: function() {
+    
+  }
+});
+
+// VIEWS
+
+var AppView = Backbone.View.extend({
+  el: $('body'),
+  initialize: function() {
+    alert('AppView init');
+  },
+  render: function() {
+    alert('AppView render');
+    
+    return this;
+  }
+});
+
+// Must be init'd with an el.
+var ButtonView = Backbone.View.extend({
+  initialize: function() {
+    
+  },
+  // maybe replace with manual setting of events, after determining mouse or
+  // touch events.
+  events: {
+    'touchstart': 'touchstart'
+  },
+  touchstart: function() {
+    
+  }
+});
+
+// Each of the pages.
+
+/**
+ * This is for the main pages that the app needs to have transitions between.
+ * I could alternatively mixin the transition functionality.
+ */
+var PageView = Backbone.View.extend({
+  transitionIn: function() {
+    
+  },
+  transitionOut: function() {
+    
+  }
+});
+
+/**
+ * The main page shown when the app is opened to to default page.
+ */
+SplashPageView = PageView.extend({
+  initialize: function() {
+    
+  }
+});
+
+/**
+ * This page shows the link to either send manually or automatically to the
+ * user's friend.
+ */
+SendLinkView = PageView.extend({
+  initialize: function() {
+    
+  }
+});
+
+/**
+ * This page is for displaying the tracking data.
+ */
+TrackingPageView = PageView.extend({
+  initialize: function() {
+    
+  }
+});
+
+/**
+ * This shows debug information of the GPS position and the orientation and
+ * compass data.
+ */
+DebugView = Backbone.View.extend({
+  initialize: function() {
+    // need to get ahold of the LocationData model.
+  }
+});
+
+// ROUTER
+
+var AppRouter = Backbone.Router.extend({
+  initialize: function() {
+    this.appView = new AppView
+  },
+  routes: {
+    '': 'index'
+  },
+  index: function() {
+    this.appView.render();
+  }
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  new AppRouter();
+  // TODO: Re-enable pushstate history for when not loading off of client.
+  //Backbone.history.start({pushState: true, root: "/Users/tarmst/code/locator/client/"});
+  Backbone.history.start();
+}, false);
